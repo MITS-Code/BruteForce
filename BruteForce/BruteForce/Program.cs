408c7687bc4ebd4fff3b6c5e63492e1c6ba519f3
Grant Visser <grantrvisser@gmail.com>
2017-07-31 10:32:25 +0200

    Cleaning Up

--- BruteForce/BruteForce/Program.cs
+++ BruteForce/BruteForce/Program.cs
@@ -11,16 +11,27 @@ namespace BruteForce
         static void Main(string[] args)
         {
             int[] testArray = new int[1000];
+            Console.Out.WriteLine("Populating array with Random integers...");
+            Random rng = new Random();
             for (int i = 0; i < testArray.Length; i++)
             {
-                Random rng = new Random();
                 testArray[i] = rng.Next(-100,100);
-                Console.Out.WriteLine(i + " " + testArray[i]);
             }
+            Console.Out.WriteLine("Populated " + testArray.Length + " values");
             BruteForceBasic bfb = new BruteForceBasic();
-            //TODO Implement Quadratic
-            //TODO Implement Linear
-            Console.Out.WriteLine("Max Subsequence Sum: " + MaxSubSequenceSum(testArray));
+            QuadraticBruteForce qbf = new QuadraticBruteForce();
+            LinearBruteForce lbf = new LinearBruteForce();
+            Console.Out.WriteLine("===============");
+            Console.Out.WriteLine("Processing...");
+            for (int i = 0; i < 10; i++)
+            {
+                bfb.MaxSubSequenceSum(testArray);
+                qbf.MaxSubSequenceSum(testArray);
+                lbf.MaxSubSequenceSum(testArray);
+            }
+            Console.Out.WriteLine("Brute Force Basic     |Max Subsequence Sum: " + bfb.MaxSubSequenceSum(testArray) + " - Average Time taken: " + bfb.getTime() + "ms - O(N^3)");
+            Console.Out.WriteLine("Quadratic Brute Force |Max Subsequence Sum: " + qbf.MaxSubSequenceSum(testArray) + " - Average Time taken: " + qbf.getTime() + "ms - O(N^2)");
+            Console.Out.WriteLine("Linear Brute Force    |Max Subsequence Sum: " + lbf.MaxSubSequenceSum(testArray) + " - Average Time taken: " + lbf.getTime() + "ms - O(N)");
             Console.ReadKey();
         }
 
--- BruteForce/BruteForce/QuadraticBruteForce.cs
+++ BruteForce/BruteForce/QuadraticBruteForce.cs
@@ -12,9 +12,12 @@ namespace BruteForce
         int maxSum = 0;//Init
         private int seqStart { get; set; }
         private int seqEnd { get; set; }
+        private int timesRun = 0;
+        private long totalTime = 0;
         private Stopwatch sw = new Stopwatch();
         public int MaxSubSequenceSum(int[] input)
         {
+            timesRun += 1;
             sw.Start();
             for (int i = 0; i < input.Length; i++)
             {
@@ -31,12 +34,14 @@ namespace BruteForce
                 }
             }
             sw.Stop();
+            totalTime += sw.ElapsedMilliseconds;
             return maxSum;
         }
 
         public long getTime()
         {
-            return sw.ElapsedMilliseconds;
+            long o = totalTime / timesRun;
+            return o;
         }
     }
 }
