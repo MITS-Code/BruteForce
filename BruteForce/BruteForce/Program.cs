4f53c67c2e1b1cb3c3c48defd2d2b9d23836540b
Grant Visser <grantrvisser@gmail.com>
2017-07-31 10:02:04 +0200

    Creation of base project

---
+++ BruteForce/BruteForce/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BruteForce
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] testArray = new int[1000];
+            for (int i = 0; i < testArray.Length; i++)
+            {
+                Random rng = new Random();
+                testArray[i] = rng.Next(-100,100);
+                Console.Out.WriteLine(i + " " + testArray[i]);
+            }
+            BruteForceBasic bfb = new BruteForceBasic();
+            //TODO Implement Quadratic
+            //TODO Implement Linear
+            Console.Out.WriteLine("Max Subsequence Sum: " + MaxSubSequenceSum(testArray));
+            Console.ReadKey();
+        }
+
+        
+    }
+}
