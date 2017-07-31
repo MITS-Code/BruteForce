952ce9c4a363e7c7ae6825eaa549cf834838c981
Grant Visser <grantrvisser@gmail.com>
2017-07-31 10:02:45 +0200

    Addition of QuadraticBruteForce

---
+++ BruteForce/BruteForce/QuadraticBruteForce.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BruteForce
+{
+    class QuadraticBruteForce //N^2 Complexity
+    {
+        int maxSum = 0;//Init
+        private int seqStart { get; set; }
+        private int seqEnd { get; set; }
+        private Stopwatch sw = new Stopwatch();
+        public int MaxSubSequenceSum(int[] input)
+        {
+            sw.Start();
+            for (int i = 0; i < input.Length; i++)
+            {
+                int thisSum = 0;
+                for (int j = i; j < input.Length; j++)
+                {
+                    thisSum += input[j];
+                    if (thisSum > maxSum)
+                    {
+                        maxSum = thisSum;
+                        seqStart = i;
+                        seqEnd = j;
+                    }
+                }
+            }
+            sw.Stop();
+            return maxSum;
+        }
+
+        public long getTime()
+        {
+            return sw.ElapsedMilliseconds;
+        }
+    }
+}
